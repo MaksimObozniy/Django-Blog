8fa9818e9db93b6c3dafdce70611efe70d2b592a
MaksimObozniy <mobozn@gmail.com>
2025-01-13 15:14:49 +0400

    changed settings for static

--- sensive_blog/settings.py
+++ sensive_blog/settings.py
@@ -100,7 +100,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/js'),
     os.path.join(BASE_DIR, 'media'),
     os.path.join(BASE_DIR, 'static/img'),
-    os.path.join(BASE_DIR, 'fonts'),
+    os.path.join(BASE_DIR, 'static/fonts'),
 ]
 
 MEDIA_ROOT = os.getenv("MEDIA_ROOT", os.path.join(BASE_DIR, 'media'))
