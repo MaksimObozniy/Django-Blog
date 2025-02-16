46244e924698342942b4dcdf568dfbdb66293204
MaksimObozniy <mobozn@gmail.com>
2025-02-16 17:37:11 +0400

    update STATICFILES_DIRS in settings

--- sensive_blog/settings.py
+++ sensive_blog/settings.py
@@ -95,19 +95,17 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'static/css'),
     os.path.join(BASE_DIR, 'static/js'),
-    os.path.join(BASE_DIR, 'media'),
     os.path.join(BASE_DIR, 'static/img'),
     os.path.join(BASE_DIR, 'static/fonts'),
 ]
 
-MEDIA_ROOT = os.getenv("MEDIA_ROOT", os.path.join(BASE_DIR, 'media'))
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_ROOT = os.getenv("STATIC_ROOT")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = os.getenv('MEDIA_URL', '/media/')
+MEDIA_URL = '/media/'
 
 INTERNAL_IPS = [
     '127.0.0.1',
